b0dff3090538d5c7aa59cdd7544de4e656fb27f6
Xiao Du <xiaodu910@gmail.com>
2018-04-01 15:45:50 -0700

    initial commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Xiao Du on 4/1/18.
+//  Copyright © 2018 Xiao Du. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    //MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
+}
+
